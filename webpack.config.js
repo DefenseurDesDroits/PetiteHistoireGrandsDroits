d7dbd62177fc8719af9fb0e21a652ed35071932a
Yorick <yorick@ctrlaltdev.xyz>
2018-06-27 23:35:52 -0700

    WIP: style

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/js/main.js'),
   output: {
     path: path.resolve(__dirname, 'public/js'),
     filename: 'bundle.js'
-  },
-  node: {
-    'dns': 'empty',
-    'fs': 'empty',
-    'tls': 'empty',
-    'net': 'empty'
   }
-};
+}
