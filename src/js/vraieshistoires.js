7b084ad0b6b8fcba5ec447e0113fc475a2296828
Yorick <yorick@ctrlaltdev.xyz>
2018-07-04 17:58:11 -0700

    add feature

--- src/js/vraieshistoires.js
+++ src/js/vraieshistoires.js
@@ -1,8 +1,10 @@
 console.warn("I am self aware")
+let timestamp = Math.floor((new Date()).getTime() / 1000)
 
 let storiesButtons = document.querySelectorAll('.stories__stories-button');
 [].forEach.call(storiesButtons, (button) => {
   button.addEventListener('click', selectStory)
+  releaseStory(button)
 })
 
 function selectStory(e) {
@@ -10,8 +12,23 @@ function selectStory(e) {
     document.querySelector('.stories__stories-button.selected').classList.remove('selected')
   }
   e.target.classList.add('selected')
+
+  if(document.querySelector('.stories__story.active')) {
+    document.querySelector('.stories__story.active').classList.replace('active', 'hidden')
+  }
+  document.querySelector('#'+e.target.parentNode.id+'-story').classList.replace('hidden', 'active')
+  updateSize()
 }
 
-setInterval(function() {
-  window.parent.postMessage({height: document.querySelector('body').clientHeight}, "*");
-},5000);
+function updateSize() {
+  window.parent.postMessage({height: document.querySelector('body').clientHeight}, "*")
+}
+
+function releaseStory(element) {
+  let releaseDate = element.getAttribute('data-release')
+  if (releaseDate <= timestamp) {
+    element.disabled = false
+  }
+}
+
+updateSize()
